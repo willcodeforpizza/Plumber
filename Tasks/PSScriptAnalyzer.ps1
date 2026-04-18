5a190a18fb8a08dde33e7ea5d7f6da885931f8c4
Martin Howlett <martin.howlett@gmail.com>
2026-04-18 02:38:47 +0100

    Add feature to customize PSSA settings by psd1 in root

--- Tasks/PSScriptAnalyzer.ps1
+++ Tasks/PSScriptAnalyzer.ps1
@@ -3,8 +3,10 @@
     Validates PSScriptAnalyzer passes
 #>
 task -Name PSScriptAnalyzer -Jobs SetVariables, {
+    $settingsPath = Join-Path $BuildRoot 'PSScriptAnalyzerSettings.psd1'
+    $settingsSplat = if (Test-Path $settingsPath) { @{ Settings = $settingsPath } } else { @{} }
     $scriptFailures = $script:moduleFolders |
-    ForEach-Object { Invoke-ScriptAnalyzer $_ }
+    ForEach-Object { Invoke-ScriptAnalyzer $_ @settingsSplat }
     $failures = foreach ($failure in $scriptFailures) {
         (
             "$($failure.ScriptName):$($failure.Line) - " +
